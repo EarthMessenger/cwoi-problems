81f489c862a8291554e778b9bc0a9a69b6afb530
earthmessenger <me@earthmessenger.xyz>
2024-10-29 12:05:46 +0800

    upd: contests.json 2024-10-29
    crawler.sh: merge old data on new data

--- crawler.sh
+++ crawler.sh
@@ -2,6 +2,6 @@
 . .env.local
 
 python scripts/crawler.py ${CWOI_NAME} ${CWOI_PASSWORD} ${CWOI_HOST} --sleep 0 > ./latest_crawl.json
-python scripts/merge.py ./src/data/contests.json ./latest_crawl.json > ./merged.json
+python scripts/merge.py ./latest_crawl.json ./src/data/contests.json > ./merged.json
 cp ./merged.json ./src/data/contests.json
 rm ./latest_crawl.json ./merged.json
